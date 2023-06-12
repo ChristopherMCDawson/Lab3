02d7887cb03b398f530ceac88161d008f01f16c2
ChristopherMCDawson <126995245+ChristopherMCDawson@users.noreply.github.com>
2023-06-11 23:03:23 -0400

    Still Launch to a blank browser

--- Lab3/Models/Person.cs
+++ Lab3/Models/Person.cs
@@ -2,11 +2,11 @@
 {
     public class Person
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public int Age { get; set; }
-        public string EmailAddress { get; set; }
-        public string Password { get; set; }
-        public string Description { get; set; }
+        public string? EmailAddress { get; set; }
+        public string? Password { get; set; }
+        public string? Description { get; set; }
     }
 }
