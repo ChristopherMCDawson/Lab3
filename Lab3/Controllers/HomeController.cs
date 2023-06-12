3f0a87514a1b0545adc3f19e177db8738dcb2d43
ChristopherMCDawson <126995245+ChristopherMCDawson@users.noreply.github.com>
2023-06-12 14:51:02 -0400

    Working away , fixing syntex

--- Lab3/Controllers/HomeController.cs
+++ Lab3/Controllers/HomeController.cs
@@ -1,84 +1,35 @@
 ﻿using Lab3.Models;
 using Microsoft.AspNetCore.Mvc;
-using System;
 
 namespace Lab3.Controllers
 {
     public class HomeController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
         public IActionResult SongForm() => View();
 
         [HttpPost]
-        public IActionResult Sing(int monkeys)
+        public IActionResult Sing(IFormCollection collection)
         {
-            try
-            {
-                if (monkeys >= 50 && monkeys <= 100)
-                {
-                    HttpContext.Session.SetInt32("Monkeys", monkeys);
-                    return RedirectToAction("Sing");
-                }
-                else
-                {
-                    ModelState.AddModelError("", "Please enter a number between 50 and 100.");
-                    return View("SongForm", monkeys);
-                }
-            }
-            catch (Exception)
-            {
-                ViewBag.ErrorMessage = "An error occurred while processing your request.";
-                // Log the exception or perform additional error handling if needed
-                return View("Error");
-            }
-
+            ViewBag.monkeyCount = Int32.Parse(collection["monkey-count"]);
+            return View("Sing");
         }
-        public IActionResult Sing()
+
+        public IActionResult CreateStudent() => View();
+
+        [HttpPost]
+        public IActionResult DisplayStudent(Person person)
         {
-            int? monkeys = HttpContext.Session.GetInt32("Monkeys");
-            ViewBag.Monkeys = monkeys ?? 0;
+            ViewBag.person = person;
+            return View("DisplayPerson");
+        }
+        public IActionResult Error()
+        {
             return View();
         }
 
-        public IActionResult CreatePerson()
-        {
-            try
-            {
-                return View();
-            }
-            catch (Exception)
-            {
-                ViewBag.ErrorMessage = "An error occurred while processing your request.";
-                // Log the exception or perform additional error handling if needed
-                return View("Error");
-            }
-        }
-
-        [HttpPost]
-        public IActionResult DisplayPerson(Person person)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    return RedirectToAction("DisplayPerson", person);
-                }
-
-                return View(person);
-            }
-            catch (Exception)
-            {
-                ViewBag.ErrorMessage = "An error occurred while processing your request.";
-                // Log the exception or perform additional error handling if needed
-                return View("Error");
-            }
-
-
-        }
-
-
     }
-
-
-
-
 }
